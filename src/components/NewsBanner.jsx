c45d82e3cf7fee145e45b8f8bc40006e886b890a
Matheus Gurgel <88843455+mathgurgel@users.noreply.github.com>
2024-08-20 00:16:32 +0000

    feat: add news banner

--- src/components/NewsBanner.jsx
+++ src/components/NewsBanner.jsx
@@ -5,19 +5,22 @@ function NewsBanner() {
   return (
     <section id="news-banner">
       <NewsComponent
-        title=""
-        description=""
-        image=""
+        title="Guerri explica por que acompanhou FURIA em Cologne"
+        description="Após seis anos como coach, veterano foi promovido a head de esports"
+        image="https://img-cdn.hltv.org/gallerypicture/SnUAZeF02_zuwY_LAN2STx.jpg?ixlib=java-2.1.0&w=1200&s=b87d9d29dbe0dce092cf170d2c75ca3e"
+        link="https://www.dust2.com.br/noticias/51885/guerri-explica-por-que-acompanhou-furia-em-cologne"
       />
       <NewsComponent
-        title=""
-        description=""
-        image=""
+        title="FURIA pode apresentar nível muito mais alto, diz Sidde"
+        description="Treinador falou da campanha na IEM Cologne e avaliou desempenho individual da equipe"
+        image="https://img-cdn.hltv.org/gallerypicture/d2eBB0uiTtJ8VgSrJIhhWW.jpg?ixlib=java-2.1.0&w=1200&s=259a7cedc081d50cdb1446e650ae883e"
+        link="https://www.dust2.com.br/noticias/51667/furia-pode-apresentar-nivel-muito-mais-alto-diz-sidde"
       />
       <NewsComponent
-        title=""
-        description=""
-        image=""
+        title="FURIA perde para Liquid e está fora da IEM Cologne"
+        description="Equipe brasileira perdeu por 2 a 0"
+        image="https://img-cdn.hltv.org/gallerypicture/L3jVdJaPi5bFpcqw0NEP9q.jpg?ixlib=java-2.1.0&w=1200&s=2a0beb98fc7a5f8e225df9a48047e4a3"
+        link="https://www.dust2.com.br/noticias/51661/furia-perde-para-liquid-e-esta-fora-da-iem-cologne"
       />
     </section>
   );
--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -1,9 +1,11 @@
 import "./NewsComponent.css";
+import { TbExternalLink } from "react-icons/tb";
 
 function NewsComponent(props) {
   let newsTitle = props.title;
   let newsDescription = props.description;
   let newsImage = props.image;
+  let newsLink = props.link;
 
   return (
     <div className="news">
@@ -13,6 +15,12 @@ function NewsComponent(props) {
       <div className="news-content">
         <h1 className="news-title">{newsTitle}</h1>
         <p className="news-description">{newsDescription}</p>
+        <div className="news-link">
+          <a href={newsLink} target="_blank"> Notícia completa </a>
+          <a href={newsLink} target="_blank">
+            <TbExternalLink />
+          </a>
+        </div>
       </div>
     </div>
   );
